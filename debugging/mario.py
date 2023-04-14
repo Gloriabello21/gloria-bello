47849a28d59cc99d67ab650bc752f62622c0e791
gloria bello <calderonscarlette69@gmail.com>
2023-04-13 20:45:22 -0400

    a

--- debugging/mario.py
+++ debugging/mario.py
@@ -2,25 +2,13 @@
 def main():
     height= int(input("Height:"))
     pyramidn(height)
+
 def pyramidn(n):
     for i in range(n):
-        print("#" * i)
-        
-if__name_ == "_main_":
-main() 
-
-#ejemplo:2
-def main():
-    height= int(input("Height:"))
-    pyramid(height)
-
-def pyramid(n):
-    for i in range(n):
-        print(i, end="")
-        print("#" * i)
+        print("#" * (i + 1))
 
 if __name__ == "__main__":
-     main()
+    main()        
 
 
 
---
+++ lecture5-unit tests/test_calculator.py
@@ -0,0 +1,10 @@
+#ejemplo1
+#este ejemplo se ve con pytest test_calculator.py
+from calculator import square 
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
